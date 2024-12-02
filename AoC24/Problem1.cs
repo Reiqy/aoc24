073a38e8bfca7078f75182416f503a71377a62a3
Reiqy <45209567+Reiqy@users.noreply.github.com>
2024-12-02 07:26:52 +0100

    Project setup and solving problem 1

---
+++ AoC24/Problem1.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC24;
+
+public class Problem1
+{
+    public (List<int> Left, List<int> Right) ReadNumbers()
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+
+        var lines = File.ReadAllLines("input/aoc24_1a.txt");
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[1]));
+        }
+
+        return (left, right);
+    }
+
+    public int SolveA()
+    {
+        var (left, right) = this.ReadNumbers();
+
+        var result = left.Order()
+            .Zip(right.Order())
+            .Sum(x => Math.Abs(x.First - x.Second));
+
+        return result;
+    }
+
+    public int SolveB()
+    {
+        var (left, right) = this.ReadNumbers();
+
+        var counts = new Dictionary<int, int>();
+
+        foreach (var number in left)
+        {
+            counts.TryAdd(number, 0);
+        }
+
+        foreach (var number in right)
+        {
+            if (counts.ContainsKey(number))
+            {
+                counts[number]++;
+            }
+        }
+
+        var result = counts.Sum(x => x.Key * x.Value);
+        return result;
+    }
+}
---
+++ AoC24/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AoC24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var problem1 = new Problem1();
+        Console.WriteLine($"Problem 1A: {problem1.SolveA()}");
+        Console.WriteLine($"Problem 1B: {problem1.SolveB()}");
+    }
+}
