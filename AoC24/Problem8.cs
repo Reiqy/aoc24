27af3a43e3a4db9165fff17b54e772d5504593b0
Reiqy <45209567+Reiqy@users.noreply.github.com>
2024-12-10 20:51:06 +0100

    Solve problem 8

---
+++ AoC24/Problem8.cs
@@ -0,0 +1,162 @@
+﻿namespace AoC24;
+
+public class Problem8
+{
+    public int SolveA()
+    {
+        var lines = File.ReadAllLines("input/aoc24_8.txt");
+
+        var height = lines.Length;
+        var width = lines[0].Length;
+
+        var antennas = new Dictionary<char, List<Antenna>>();
+        foreach (var (y, line) in lines.Select((line, y) => (y, line)))
+        {
+            foreach (var (x, symbol) in line.Select((symbol, x) => (x, symbol)))
+            {
+                if (symbol == '.')
+                {
+                    continue;
+                }
+
+                var antennaSymbol = symbol;
+                if (!antennas.ContainsKey(antennaSymbol))
+                {
+                    antennas.Add(antennaSymbol, new List<Antenna>());
+                }
+
+                antennas[antennaSymbol].Add(new Antenna(x, y));
+            }
+        }
+
+        var antinodes = new HashSet<(int, int)>();
+        foreach (var antennaCategory in antennas)
+        {
+            var category = antennaCategory.Key;
+            var categoryAntennas = antennaCategory.Value;
+            foreach (var (i, firstAntenna) in categoryAntennas.Select((a, i) => (i, a)))
+            {
+                foreach (var (j, secondAntenna) in categoryAntennas.Select((a, j) => (j, a)))
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    // B - A = A -> B
+                    // first - second = second -> first
+                    var antennaVectorX = firstAntenna.X - secondAntenna.X;
+                    var antennaVectorY = firstAntenna.Y - secondAntenna.Y;
+
+                    var firstAntinodeX = firstAntenna.X + antennaVectorX;
+                    var firstAntinodeY = firstAntenna.Y + antennaVectorY;
+
+                    if (firstAntinodeX >= 0 && firstAntinodeX < width && firstAntinodeY >= 0 && firstAntinodeY < height)
+                    {
+                        antinodes.Add((firstAntinodeX, firstAntinodeY));
+                    }
+
+                    var secondAntinodeX = secondAntenna.X - antennaVectorX;
+                    var secondAntinodeY = secondAntenna.Y - antennaVectorY;
+
+                    if (secondAntinodeX >= 0 && secondAntinodeX < width && secondAntinodeY >= 0 && secondAntinodeY < height)
+                    {
+                        antinodes.Add((secondAntinodeX, secondAntinodeY));
+                    }
+                }
+            }
+        }
+
+        return antinodes.Count;
+    }
+
+    private record Antenna(int X, int Y);
+
+    public int SolveB()
+    {
+        var lines = File.ReadAllLines("input/aoc24_8.txt");
+
+        var height = lines.Length;
+        var width = lines[0].Length;
+
+        var antennas = new Dictionary<char, List<Antenna>>();
+        foreach (var (y, line) in lines.Select((line, y) => (y, line)))
+        {
+            foreach (var (x, symbol) in line.Select((symbol, x) => (x, symbol)))
+            {
+                if (symbol == '.')
+                {
+                    continue;
+                }
+
+                var antennaSymbol = symbol;
+                if (!antennas.ContainsKey(antennaSymbol))
+                {
+                    antennas.Add(antennaSymbol, new List<Antenna>());
+                }
+
+                antennas[antennaSymbol].Add(new Antenna(x, y));
+            }
+        }
+
+        var antinodes = new HashSet<(int, int)>();
+        foreach (var antennaCategory in antennas)
+        {
+            var category = antennaCategory.Key;
+            var categoryAntennas = antennaCategory.Value;
+            foreach (var (i, firstAntenna) in categoryAntennas.Select((a, i) => (i, a)))
+            {
+                foreach (var (j, secondAntenna) in categoryAntennas.Select((a, j) => (j, a)))
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    // B - A = A -> B
+                    // first - second = second -> first
+                    var antennaVectorX = firstAntenna.X - secondAntenna.X;
+                    var antennaVectorY = firstAntenna.Y - secondAntenna.Y;
+                    var divisor = GCD(antennaVectorX, antennaVectorY); // Is this even needed?
+
+                    var antennaStepVectorX = antennaVectorX / divisor;
+                    var antennaStepVectorY = antennaVectorY / divisor;
+
+                    var antinodeX = firstAntenna.X;
+                    var antinodeY = firstAntenna.Y;
+                    while (antinodeX >= 0 && antinodeX < width && antinodeY >= 0 && antinodeY < height)
+                    {
+                        antinodes.Add((antinodeX, antinodeY));
+
+                        antinodeX += antennaStepVectorX;
+                        antinodeY += antennaStepVectorY;
+                    }
+
+                    antinodeX = firstAntenna.X;
+                    antinodeY = firstAntenna.Y;
+                    while (antinodeX >= 0 && antinodeX < width && antinodeY >= 0 && antinodeY < height)
+                    {
+                        antinodes.Add((antinodeX, antinodeY));
+
+                        antinodeX -= antennaStepVectorX;
+                        antinodeY -= antennaStepVectorY;
+                    }
+                }
+            }
+        }
+
+        return antinodes.Count;
+    }
+
+    private static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return Math.Abs(a);
+    }
+}
--- AoC24/Program.cs
+++ AoC24/Program.cs
@@ -8,33 +8,37 @@ internal class Program
     {
         var startTimestamp = Stopwatch.GetTimestamp();
 
-        var problem1 = new Problem1();
-        Console.WriteLine($"Problem 1A: {problem1.SolveA()}");
-        Console.WriteLine($"Problem 1B: {problem1.SolveB()}");
+        //var problem1 = new Problem1();
+        //Console.WriteLine($"Problem 1A: {problem1.SolveA()}");
+        //Console.WriteLine($"Problem 1B: {problem1.SolveB()}");
 
-        var problem2 = new Problem2();
-        Console.WriteLine($"Problem 2A: {problem2.SolveA()}");
-        Console.WriteLine($"Problem 2B: {problem2.SolveB()}");
+        //var problem2 = new Problem2();
+        //Console.WriteLine($"Problem 2A: {problem2.SolveA()}");
+        //Console.WriteLine($"Problem 2B: {problem2.SolveB()}");
 
-        var problem3 = new Problem3();
-        Console.WriteLine($"Problem 3A: {problem3.SolveA()}");
-        Console.WriteLine($"Problem 3B: {problem3.SolveB()}");
+        //var problem3 = new Problem3();
+        //Console.WriteLine($"Problem 3A: {problem3.SolveA()}");
+        //Console.WriteLine($"Problem 3B: {problem3.SolveB()}");
 
-        var problem4 = new Problem4();
-        Console.WriteLine($"Problem 4A: {problem4.SolveA()}");
-        Console.WriteLine($"Problem 4B: {problem4.SolveB()}");
+        //var problem4 = new Problem4();
+        //Console.WriteLine($"Problem 4A: {problem4.SolveA()}");
+        //Console.WriteLine($"Problem 4B: {problem4.SolveB()}");
 
-        var problem5 = new Problem5();
-        Console.WriteLine($"Problem 5A: {problem5.SolveA()}");
-        Console.WriteLine($"Problem 5B: {problem5.SolveB()}");
+        //var problem5 = new Problem5();
+        //Console.WriteLine($"Problem 5A: {problem5.SolveA()}");
+        //Console.WriteLine($"Problem 5B: {problem5.SolveB()}");
 
-        var problem6 = new Problem6();
-        Console.WriteLine($"Problem 6A: {problem6.SolveA()}");
-        Console.WriteLine($"Problem 6B: {problem6.SolveB()}");
+        //var problem6 = new Problem6();
+        //Console.WriteLine($"Problem 6A: {problem6.SolveA()}");
+        //Console.WriteLine($"Problem 6B: {problem6.SolveB()}");
 
-        var problem7 = new Problem7();
-        Console.WriteLine($"Problem 7A: {problem7.SolveA()}");
-        Console.WriteLine($"Problem 7B: {problem7.SolveB()}");
+        //var problem7 = new Problem7();
+        //Console.WriteLine($"Problem 7A: {problem7.SolveA()}");
+        //Console.WriteLine($"Problem 7B: {problem7.SolveB()}");
+
+        var problem8 = new Problem8();
+        Console.WriteLine($"Problem 8A: {problem8.SolveA()}");
+        Console.WriteLine($"Problem 8B: {problem8.SolveB()}");
 
         var elapsed = Stopwatch.GetElapsedTime(startTimestamp);
         Console.WriteLine($"Total elapsed time: {elapsed}");
